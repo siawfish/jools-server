ad454cd12c7cbd22df0d92d3a823d72fb743b99b
Gerald <mcamanor@gmail.com>
2025-04-19 22:07:45 +0000

    Add pushNotification setting to users table and include user settings in worker retrieval functions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -19,6 +19,7 @@ export const usersTable = pgTable("users", {
             email: true,
             sms: true,
             pushToken: "",
+            pushNotification: false,
         },
         theme: Theme.LIGHT,
         language: LanguageType.ENGLISH,
